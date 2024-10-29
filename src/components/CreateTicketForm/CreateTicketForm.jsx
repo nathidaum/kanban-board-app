bcede3b65ad09271759554a3dc4379c80aeb44ce
nathidaum <nathidaum@gmail.com>
2024-10-29 18:34:24 +0100

    Tweak ticket form design

--- src/components/CreateTicketForm/CreateTicketForm.jsx
+++ src/components/CreateTicketForm/CreateTicketForm.jsx
@@ -37,10 +37,11 @@ const CreateTicketForm = ({ callBacktoCreateTicket }) => {
 
   return (
     <form onSubmit={handleSubmit} className="form">
-      <h2>Create New Ticket</h2>
+      <h2>Create a new ticket</h2>
       <div className="inputs">
         <label>
-          Title:
+          TITLE
+          <br></br>
           <input
             type="text"
             placeholder="Do something..."
@@ -51,53 +52,61 @@ const CreateTicketForm = ({ callBacktoCreateTicket }) => {
         </label>
 
         <label>
-          Description:
-          <input
-            type="text"
-            placeholder="Here are some details..."
+          DESCRIPTION
+          <br />
+          <textarea
+            className="textarea"
+            placeholder="Here are some details about the ticket so that..."
             value={description}
             onChange={(e) => setDescription(e.target.value)}
             required
+            rows="5" // Controls the height of the textarea
+            cols="40" // Controls the width of the textarea
           />
         </label>
 
         <label>
-          Assignee:
+          ASSIGNEE
+          <br></br>
           <input
             type="text"
-            placeholder="Alice"
+            placeholder="Nathi"
             value={assignee}
             onChange={(e) => setAssignee(e.target.value)}
             required
           />
         </label>
 
-        <label>
-          Priority:
-          <select
-            value={priority}
-            onChange={(e) => setPriority(e.target.value)}
-            required
-          >
-            <option value="">Select Priority</option>
-            <option value="Low">Low</option>
-            <option value="Medium">Medium</option>
-            <option value="High">High</option>
-          </select>
-        </label>
+        <div className="horizontal">
+          <label>
+            PRIORITY
+            <br></br>
+            <select
+              value={priority}
+              onChange={(e) => setPriority(e.target.value)}
+              required
+            >
+              <option value="">Select Priority</option>
+              <option value="Low">Low</option>
+              <option value="Medium">Medium</option>
+              <option value="High">High</option>
+            </select>
+          </label>
 
-        <label>
-          Due Date:
-          <input
-            type="date"
-            value={dueDate}
-            onChange={(e) => setDueDate(e.target.value)}
-            required
-            min={today} // Set the minimum date to today
-          />
-        </label>
+          <label>
+            DUE DATE
+            <br></br>
+            <input
+              type="date"
+              value={dueDate}
+              onChange={(e) => setDueDate(e.target.value)}
+              required
+              min={today} // Set the minimum date to today
+            />
+          </label>
+        </div>
       </div>
-      <button type="submit">Create Ticket</button>
+      <button className="submit" type="submit">Create ticket</button>
     </form>
   );
 };
