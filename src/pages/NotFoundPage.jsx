cb5514512d6f7b00489bdf0662a58c6e630f5f01
nathidaum <nathidaum@gmail.com>
2024-10-31 15:23:25 +0100

    Design changes

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -9,7 +9,7 @@ function NotFound() {
         <h1>Oooooops! <br></br> This page does not exist. 🕵️</h1>
         <h3 className="grey">Too bad! I can help you get back to the homepage though. </h3>
         <Link to="/">
-          <button className="secondary-button">Go to homepage</button>
+          <button>Go to homepage</button>
         </Link>
       </div>
     </div>
