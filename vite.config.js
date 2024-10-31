a9f058a3b3ba4f7200decddef3d209993f9a687e
nathidaum <nathidaum@gmail.com>
2024-10-31 09:08:43 +0100

    Remove base url again

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/kanban-board-app/', // Setting the base path for GitHub Pages
   plugins: [react()],
 })
