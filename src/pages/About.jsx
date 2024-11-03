f81489129271142127a65c0a7635ae077519e6ac
nathidaum <nathidaum@gmail.com>
2024-11-03 13:17:42 +0100

    Adjust responsive designs

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ function About() {
         <div className="intro">
             <h1>Hi, I am Nathi. 👋</h1>
             <h3>I've built this Kanban to help you keep track of your tasks.</h3>
-            <p>In case you want to check out the code, feel free to have a look at my GitHub repository. It is supposed to help you keep track of your tasks. Feel free to add new tasks and manage them by state. Don't forget to tackle the most important ones first. For that, please take note of their priority label and due date. Have fun!</p>
+            <p>Feel free to add new tasks and manage them by state. Don't forget to tackle the most important ones first. For that, please take note of their priority label and due date. Have fun!</p>
             <button><a href="https://github.com/nathidaum/kanban-board-app">See code</a></button>
         </div>
       </div>
